5d7dc31f108de6f87507197d33e29336ed7316e9
nelapudisandeep <nelapudisandeep@gmail.com>
2020-02-29 19:29:48 +0530

    port from environmental variable

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT||1111;
+const PORT = process.env.PORT||3000;
 const Datastore = require('nedb');
 const database = new Datastore('database.db');
 database.loadDatabase();
